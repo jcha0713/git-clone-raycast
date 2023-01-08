68097b69585924b82d8dca3ff348c3b7e3c7be1d
jcha0713 <joocha0713@gmail.com>
2023-01-07 21:05:19 -0800

    Initial version

---
+++ git-clone.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title git clone
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 📦
+# @raycast.argument1 { "type": "text", "placeholder": "directory" }
+
+# Documentation:
+# @raycast.description run git clone with copied link
+# @raycast.author Joohoon Cha
+# @raycast.authorURL https://github.com/jcha0713
+
+dir="practice"
+year=$(date +%Y)
+
+if [[ -n "$1" ]]; then
+  dir=$1
+fi
+
+cd ~/jhcha/dev/$year/$dir
+git clone $(pbpaste)
+
